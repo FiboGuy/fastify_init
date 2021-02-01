d5eacb849b350c81e2b407b2c3d22a456c77fcd8
origin <cristian@alternaenergetica.com>
2021-02-02 00:04:23 +0100

    ongoing

--- lib/Server.ts
+++ lib/Server.ts
@@ -1,12 +1,15 @@
-import fastify from 'fastify'
-import bodyfrom from 'fastify-formbody'
+import fastify, {FastifyInstance} from 'fastify'
+import bodyform from 'fastify-formbody'
 import fastifySwagger from 'fastify-swagger'
 import cors from 'fastify-cors'
 import * as fs from 'fs'
 
 class Server{
+    private server: FastifyInstance
     public constructor(){
-
+        this.server = fastify()
+        this.server.register(bodyform)
+        this.server.register(cors)
     }
 
     
@@ -14,5 +17,19 @@ class Server{
     private registerSwagger()
     {
 
+    }
+
+    public getServer(): FastifyInstance{
+        return this.server
+    }
+
+    public listen(port: number):void{
+        this.server.listen(port, err => {
+            if(err){
+                console.log(err)
+                process.exit(1)
+            }
+            console.log(`Listening on port ${port}`)
+        })
     }
 }
